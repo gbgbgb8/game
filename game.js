50a1ed4f52ac3db727280780aacdb5a8075ee9ce
Nota Name <email@thisisnotanemail.com>
2023-07-16 09:59:21 -0700

    design changes

--- game.js
+++ game.js
@@ -1,6 +1,7 @@
 let randomNumber = Math.floor(Math.random() * 10) + 1;
 let attempts = 0;
 let guessInput = document.getElementById('guess');
+
 let winCount = localStorage.getItem('wins') || 0;
 let lossCount = localStorage.getItem('losses') || 0;
 document.getElementById('win-count').textContent = winCount;
@@ -15,42 +16,46 @@ function clearGuess() {
 }
 
 function playGame() {
-  let userGuess = guessInput.value;
-  attempts++;
+    let userGuess = guessInput.value;
+    attempts++;
 
-  let guessHistory = document.getElementById('guess-history');
-  let guessResult = document.createElement('li');
+    let guessHistory = document.getElementById('guess-history');
+    let guessResult = document.createElement('li');
 
-  if (userGuess < randomNumber) {
-    document.getElementById('result').innerHTML = "Too low! Try again.";
-    guessResult.textContent = userGuess + " - Too low";
-  } else if (userGuess > randomNumber) {
-    document.getElementById('result').innerHTML = "Too high! Try again.";
-    guessResult.textContent = userGuess + " - Too high";
-  } else {
-    document.getElementById('result').innerHTML = "Congratulations! You got it right!";
-    guessResult.textContent = userGuess + " - Correct!";
-    winCount++;
-    localStorage.setItem('wins', winCount);
-    document.getElementById('win-count').textContent = winCount;
-    endGame();
-  }
+    if (userGuess < randomNumber) {
+        document.getElementById('result').innerHTML = "Too low! Try again.";
+        guessResult.textContent = userGuess + " - Too low";
+    } else if (userGuess > randomNumber) {
+        document.getElementById('result').innerHTML = "Too high! Try again.";
+        guessResult.textContent = userGuess + " - Too high";
+    } else {
+        document.getElementById('result').innerHTML = "Congratulations! You got it right!";
+        guessResult.textContent = userGuess + " - Correct!";
+        endGame(true);
+    }
 
-  if (attempts === 3 && userGuess != randomNumber) {
-    document.getElementById('result').innerHTML = "Sorry, you didn't guess the number. The number was " + randomNumber;
-    lossCount++;
-    localStorage.setItem('losses', lossCount);
-    document.getElementById('loss-count').textContent = lossCount;
-    endGame();
-  }
+    if (attempts === 3 && userGuess != randomNumber) {
+        document.getElementById('result').innerHTML = "Sorry, you didn't guess the number. The number was " + randomNumber;
+        endGame(false);
+    }
 
-  guessHistory.appendChild(guessResult);
-  guessInput.value = ""; // clear the input for the next guess
+    guessHistory.appendChild(guessResult);
+    guessInput.value = ""; // clear the input for the next guess
 }
 
-function endGame() {
+function endGame(isWin) {
     guessInput.disabled = true; // disable input
     document.getElementById('submit-button').style.display = 'none'; // hide submit button
+
+    if (isWin) {
+        winCount++;
+        localStorage.setItem('wins', winCount);
+        document.getElementById('win-count').textContent = winCount;
+    } else {
+        lossCount++;
+        localStorage.setItem('losses', lossCount);
+        document.getElementById('loss-count').textContent = lossCount;
+    }
 
     // start the countdown
     let countdown = 3;
@@ -79,8 +84,10 @@ function resetGame() {
 }
 
 function resetScoreboard() {
-    localStorage.setItem('wins', 0);
-    localStorage.setItem('losses', 0);
-    document.getElementById('win-count').textContent = '0';
-    document.getElementById('loss-count').textContent = '0';
+    winCount = 0;
+    lossCount = 0;
+    document.getElementById('win-count').textContent = winCount;
+    document.getElementById('loss-count').textContent = lossCount;
+    localStorage.removeItem('wins');
+    localStorage.removeItem('losses');
 }
